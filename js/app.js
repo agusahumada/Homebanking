0570b595004009e83d0c640cbf3163646b107a11
agusahumada <agustinaahumada649@gmail.com>
2023-02-03 04:38:14 -0300

    New Utils file added

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
-//variables
-let balance = document.getElementById('balance-account');
-let newBalance = parseInt(balance);
-newBalance = 5000;
-balance.innerHTML = newBalance;
+const balanceElement = document.getElementById('balance-account');
+const withdrawalLimitElement = document.getElementById('withdrawal-limit');
+const withdrawBtnElement = document.getElementById('withdraw-btn');
+
+let water = 350;
+let tel = 425;
+let electricity = 210;
+let internet = 570;
+let dailyWithdraw = 0;
+let balance = 5000;
+let newWithdrawalLimit = 2000;
+balanceElement.innerHTML = balance;
+withdrawalLimitElement.innerHTML = newWithdrawalLimit;
 
 function withdrawMoney() {
     let amount = prompt('Ingrese el monto de la extracción');
     let newAmount = parseInt(amount);
-    newBalance = newBalance - newAmount;
-    balance.innerHTML = newBalance;    
+    const { response, success } = validate(newAmount, balance, dailyWithdraw, newWithdrawalLimit);
+    if (success === false) {
+        alert(response);
+        return;
+    }
+    balance = substract(balance, newAmount);
+    balanceElement.innerHTML = balance;
+    dailyWithdraw = dailyWithdraw + newAmount;
+
+    if (dailyWithdraw >= newWithdrawalLimit) {
+        withdrawBtnElement.disabled = true;
+        withdrawBtnElement.classList.add('disabled-btn');
+    }
 }
 
 function depositMoney() {
     let amount = prompt('Ingrese el monto del deposito');
     let newAmount = parseInt(amount);
-    newBalance = newBalance + newAmount;
-    balance.innerHTML = newBalance;    
+    balance = add(balance, newAmount);
+    balanceElement.innerHTML = balance;
 }
 
 function payService() {
-    
+    let payment = prompt("Ingrese el numero que corresponda con el servicio que");
 }
---
+++ js/utils.js
@@ -0,0 +1,31 @@
+function substract(num1, num2) {
+    return num1 - num2;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function validate(newAmount, balance, dailyWithdraw, newWithdrawalLimit) {
+    let success = false;
+    let response = "";
+
+    if (isNaN(newAmount)) return {response: "No se ingreso un monto", success};
+
+    if (newAmount > balance) {
+        response = 'No cuenta con saldo suciente para realizar una extracción';
+        return {response, success};
+    }
+    if (newAmount > newWithdrawalLimit) {
+        response =  `El limite de la extracción es de ${newWithdrawalLimit}`;
+        return {response, success};
+    }
+    let previewTotal = dailyWithdraw + newAmount;
+    console.log({ previewTotal, newWithdrawalLimit });
+
+    if (previewTotal > newWithdrawalLimit) {
+        response = 'Limite excedido';
+        return {response, success};
+    }
+    return {response, success: true};
+}
