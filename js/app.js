1a9196355c79432c00bde3bb48ede6d48e67e8a2
agusahumada <agustinaahumada649@gmail.com>
2023-02-08 01:52:22 -0300

    Login added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const bodyElement = document.getElementById('bg');
+const userElement = document.getElementById('user-number');
 const balanceElement = document.getElementById('balance-account');
 const withdrawalLimitElement = document.getElementById('withdrawal-limit');
 const withdrawBtnElement = document.getElementById('withdraw-btn');
@@ -13,6 +15,37 @@ let friendAcount = 1234567;
 let friendAcount2 = 89101112;
 balanceElement.innerHTML = balance;
 withdrawalLimitElement.innerHTML = newWithdrawalLimit;
+
+bodyElement.classList.add('bg-login');
+
+function login() {
+    setTimeout(() => {
+        let numberAccountUser = null;
+
+        while (numberAccountUser == null || numberAccountUser == '') {
+            numberAccountUser = prompt('Ingrese su numero de cuenta');
+            if (numberAccountUser == null || numberAccountUser == '') {
+                alert('No se ingreso ningún número de cuenta');
+            }
+        }
+        let numberRepeat = prompt('Repita su numero de cuenta');
+
+        if (numberRepeat == null || numberRepeat == '' || numberRepeat != numberAccountUser) {
+            alert('El numero ingresado no es correcto, vuelva a ingresar su número de cuenta');
+            login();
+            return;
+        } else {
+            alert('Gracias! En un momento será redirigido a su cuenta');
+
+        }
+        let parsedAccountUser = parseInt(numberAccountUser);
+        let parsedNumberRepeat = parseInt(numberRepeat);
+        bodyElement.classList.remove('bg-login');
+        userElement.innerHTML = numberAccountUser;
+    }, 300);
+}
+
+login();
 
 function withdrawMoney() {
     let amount = prompt('Ingrese el monto de la extracción');
@@ -56,26 +89,26 @@ function payService() {
             alert('No ha elegido ningun servicio');
             break;
         case '1':
-           prevBalance = substract( balance, water, 'Usted ha pagado el servicio de Agua.\n Valor: $' + water);
+            prevBalance = substract(balance, water, 'Usted ha pagado el servicio de Agua.\n Valor: $' + water);
             balance = prevBalance ? prevBalance : balance;
             break;
         case '2':
-            prevBalance = substract( balance, tel, 'Usted ha pagado el servicio de Tel.\n Valor: $' + tel);
+            prevBalance = substract(balance, tel, 'Usted ha pagado el servicio de Tel.\n Valor: $' + tel);
             balance = prevBalance ? prevBalance : balance;
             break;
         case '3':
-            prevBalance = substract( balance, electricity, 'Usted ha pagado el servicio de Electricidad.\n Valor: $' + electricity);
+            prevBalance = substract(balance, electricity, 'Usted ha pagado el servicio de Electricidad.\n Valor: $' + electricity);
             balance = prevBalance ? prevBalance : balance;
             break;
         case '4':
-            prevBalance = substract( balance, internet, 'Usted ha pagado el servicio de Internet.\n Valor: $' + internet);
+            prevBalance = substract(balance, internet, 'Usted ha pagado el servicio de Internet.\n Valor: $' + internet);
             balance = prevBalance ? prevBalance : balance;
             break;
         default:
             alert('El servicio que eligió no existe');
             break;
-        }
-        balanceElement.innerHTML = balance;
+    }
+    balanceElement.innerHTML = balance;
 }
 
 function transferMoney() {
@@ -88,17 +121,17 @@ function transferMoney() {
         return;
     }
     let numberAccount = prompt('Ingrese el numero de la cuenta que desea transferir');
-    if(numberAccount == null || numberAccount == ''){
+    if (numberAccount == null || numberAccount == '') {
         alert('No se ingreso ningún número de cuenta');
         return
     }
 
     let parsedNumberAccount = parseInt(numberAccount);
 
-    if(parsedNumberAccount == friendAcount || parsedNumberAccount == friendAcount2){
-        prevBalance = substract( balance, newAmount, 'Transferencia realizada con éxito');
+    if (parsedNumberAccount == friendAcount || parsedNumberAccount == friendAcount2) {
+        prevBalance = substract(balance, newAmount, 'Transferencia realizada con éxito');
         balance = prevBalance ? prevBalance : balance;
-    }else{
+    } else {
         alert('Solo puede transferirse dinero a una cuenta amiga.');
     }
     balanceElement.innerHTML = balance;
@@ -107,12 +140,11 @@ function transferMoney() {
 function changeLimit() {
     let amount = prompt('Ingrese el nuevo limite de extraccion');
     let newAmount = parseInt(amount);
-    if(isNaN(newAmount)){
+    if (isNaN(newAmount)) {
         alert('No se ingreso ningún monto');
         return
     }
     newWithdrawalLimit = newAmount;
     withdrawalLimitElement.innerHTML = newWithdrawalLimit;
     alert('El limite de extracción ha sido cambiado exitosamente');
-    
-}
+};
